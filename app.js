59677c85ec92931727e8d51dd699423fae515396
wildpineweb <86894890+wildpineweb@users.noreply.github.com>
2022-08-13 01:19:33 -0700

    First upload
    Let's DOUX it!

---
+++ app.js
@@ -0,0 +1,43 @@
+
+let toggleNavStatus = false;
+
+let toggleNav = function () {
+    let getSidebar = document.querySelector(".navbar-menu");
+    let getSidebarUL = document.querySelector(".side-nav ul");
+    let getSidebarLinks = document.querySelectorAll(".side-nav a");
+    let getSidebarVisibility = document.querySelector(".side-nav");
+    var htmlGrab = document.querySelector("html");
+    const hamburger = document.querySelector('.hamburger');
+
+    hamburger.classList.toggle('is-active');
+
+    if (toggleNavStatus === false) {
+
+      getSidebarVisibility.style.visibility = "visible";
+      getSidebarVisibility.style.pointerEvents = "initial";
+
+      getSidebarLinks.forEach((item, index)=>{
+        console.log(item);
+        item.style.opacity = "1";
+        item.style.visibility = "visible";
+        });
+      getSidebar.style.width = "60%";
+      htmlGrab.classList.add("clicked");
+      toggleNavStatus = true;
+      servicesUL.classList.add("clicked");
+    } 
+
+    else if (toggleNavStatus === true) {
+
+         getSidebarLinks.forEach((item, index)=>{
+          item.style.opacity = "0";
+          item.style.transitionDelay = "0s";
+          item.style.visibility = "hidden";
+         });
+        getSidebar.style.width = "0";
+        htmlGrab.classList.remove("clicked");
+        toggleNavStatus = false;
+        servicesUL.classList.remove("clicked");
+        getSidebarVisibility.style.pointerEvents = "none";
+    }
+}
